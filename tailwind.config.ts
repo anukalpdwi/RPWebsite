c2f29d29820e70e95d61e4876d024bf91b4ac74f
Anukalp Dwivedi <anukalpdwi1995@gmail.com>
2026-03-23 21:27:40 +0530

    f

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,10 +5,6 @@ export default {
   content: ["./client/index.html", "./client/src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
-      fontFamily: {
-        heading: ["Outfit", "sans-serif"],
-        sans: ["Inter", "sans-serif"],
-      },
       borderRadius: {
         lg: "var(--radius)",
         md: "calc(var(--radius) - 2px)",
